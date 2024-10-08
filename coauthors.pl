0674c42bb52e412756e165cc3980db8bdbab693a
Philip Cramer <107579314+PhilipCramer@users.noreply.github.com>
2024-10-08 22:52:02 +0200

    changed so emails are sorrounded by <  >

--- coauthors.pl
+++ coauthors.pl
@@ -9,8 +9,8 @@ coauth_list(List) :-
           ),
     split_string(Str, "\n", "", Lines),
     coauth_list_parse(Lines, List0),
-    coauth_self(Nm,Em),
-    Self = ((Nm, Em), _),
+    coauth_self(NameEmail),
+    Self = (NameEmail, _),
     (member(Self, List0) -> select(Self, List0, List)
     ; List = List0).
 
@@ -19,24 +19,25 @@ coauth_list_parse([], []).
 coauth_list_parse([""], []).
 coauth_list_parse([Line|Lines], [H|List]) :-
     split_string(Line, "\t", "", [_Commits, NameEmail]),
-    split_string(NameEmail, "<", " ", [Name, Email0]),
-    split_string(Email0, ">", " ", [Email|_]),
-    H = ((Name, Email), false),
+    H = (NameEmail, false),
     coauth_list_parse(Lines, List).
 
 % coauth_self(-Name, -Email)
-coauth_self(Name, Email) :-
+coauth_self(NameEmail) :-
     setup_call_cleanup(
         process_create(path(git), ['config', 'user.name'], [stdout(pipe(UserName))]),
         read_string(UserName, _, Name0),
         close(UserName)
         ),
-    split_string(Name0, "\n", "", [Name|_]),
+    split_string(Name0, "\n", "", [Name1|_]),
     setup_call_cleanup(
         process_create(path(git), ['config', 'user.email'], [stdout(pipe(UserEmail))]),
         read_string(UserEmail, _, Email0),
         close(UserEmail)
         ),
-    split_string(Email0, "\n", "", [Email|_]).
+    split_string(Email0, "\n", "", [Email1|_]),
+    string_concat(Name1, " <", Name2),
+    string_concat(Name2, Email1, NameEmail0),
+    string_concat(NameEmail0, ">", NameEmail).
   
 
